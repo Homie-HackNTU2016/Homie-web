81413ee561fd23031ce5d7dbe87abd122e85cac8
Thomas Lin <ithomaslin@gmail.com>
2016-08-19 22:55:19 +0800

    adding url schema for index

---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+;(function(document, window, index) {
+	$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+
+}(document, window, 0));
