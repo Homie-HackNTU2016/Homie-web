d9efbc612de1c5bc6a697eeac3fea33f4c1cab92
Thomas Lin <ithomaslin@gmail.com>
2016-08-19 23:08:50 +0800

    Fixing merge

--- Homie/urls.py
+++ Homie/urls.py
@@ -20,9 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-<<<<<<< HEAD
-    url(r'', TemplateView.as_view(template_name='index.html'))
-=======
+    url(r'', TemplateView.as_view(template_name='index.html')),
     url(r'^userp/', include('userp.urls')),
->>>>>>> 3fcfd18cf94f225cec974e8d9556a6f7c6839304
 ]
