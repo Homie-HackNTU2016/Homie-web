86b2953c6012f600f626e0aaecc058151b30ec4d
Thomas Lin <ithomaslin@gmail.com>
2016-08-19 18:57:38 +0800

    modification of the data model

--- app/models.py
+++ app/models.py
@@ -3,3 +3,25 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    admin = models.BooleanField()
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    location = models.TextField()
+    register_date = models.DateTimeField('date registered')
+    updated_date = models.DateTimeField('date updated')
+
+
+class Products(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE())
+    name = models.TextField()
+    price = models.IntegerField()
+    available = models.BooleanField()
+    amount = models.IntegerField()
+    required_hour = models.FloatField()
+    likes = models.IntegerField()
+    pictures = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    update_date = models.DateTimeField('date updated')
